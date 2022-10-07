309228a760770ba7fa7673006bdd6a64272618db
Administrator <admin@example.com>
2022-10-07 20:20:03 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Person.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+/**
+ * @author dengli
+ * @create 2022-10-07 10:54
+ */
+public class Person {
+    public void eat(){
+        System.out.println("吃饭");
+    }
+}
---
+++ Module01/src/com/atguigu/bean/Student.java
@@ -0,0 +1,8 @@
+package com.atguigu.bean;
+
+/**
+ * @author dengli
+ * @create 2022-10-07 10:59
+ */
+public class Student extends Person {
+}
